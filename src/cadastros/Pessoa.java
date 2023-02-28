8ffabcd145bc270b11d757ed65138d8eb5c3dc80
André Hopf <andre.hopf@gmail.com>
2023-02-28 11:14:03 -0300

    Adiciona classe Pessoa

---
+++ src/cadastros/Pessoa.java
@@ -0,0 +1,12 @@
+package cadastros;
+
+import java.util.Date;
+
+public abstract class Pessoa {
+    int Id;
+    String nome;
+    String genero;
+    Date dob;
+    String cpf;
+    String telefone;
+}
